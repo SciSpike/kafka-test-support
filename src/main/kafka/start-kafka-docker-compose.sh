b2c8fae9162aecd1558aca03cbcfcf1df17f3a63
Matthew Adams <matthew.adams@northscaler.com>
2020-03-23 12:50:34 -0500

    quote cmd

--- src/main/kafka/start-kafka-docker-compose.sh
+++ src/main/kafka/start-kafka-docker-compose.sh
@@ -3,5 +3,5 @@
 THIS_DIR="$(cd "$(dirname "$0")"; pwd)"
 
 CMD="docker-compose -f $KAFKA_TEST_SUPPORT_DOCKER_COMPOSE_FILE -p kafka-test-support up --quiet-pull --no-recreate --remove-orphans -d"
-echo $CMD
+echo "$CMD"
 $CMD
