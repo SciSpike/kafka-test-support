decba6ef58eea9687cf10e2d4fca0e062b8aa794
Brent Fleckner <brent.fleckner@northscaler.com>
2021-07-22 10:10:08 -0700

    minor fix

--- src/main/kafka/start-kafka-docker-compose.sh
+++ src/main/kafka/start-kafka-docker-compose.sh
@@ -2,6 +2,6 @@
 
 THIS_DIR="$(cd "$(dirname "$0")"; pwd)"
 
-CMD="docker compose -f $KAFKA_TEST_SUPPORT_DOCKER_COMPOSE_FILE -p kafka-test-support up --quiet-pull --no-recreate --remove-orphans -d"
+CMD="docker-compose -f $KAFKA_TEST_SUPPORT_DOCKER_COMPOSE_FILE -p kafka-test-support up --quiet-pull --no-recreate --remove-orphans -d"
 echo "$CMD"
 $CMD
